45081acf91fc9347b4546d53be6560f872112c58
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-08-16 23:11:25 -0400

    Update Get-DFSRQuotaSize.ps1

--- Get-DFSRQuotaSize.ps1
+++ Get-DFSRQuotaSize.ps1
@@ -1,4 +1,7 @@
-﻿#requires -Version 3.0
+﻿#requires -Modules Microsoft.PowerShell.Utility
+#requires -Version 3.0
+#requires -runasAdministrator
+
 function Get-DFSRQuotaSize
 {
   <#
@@ -7,15 +10,12 @@ function Get-DFSRQuotaSize
 
       .EXAMPLE
       Get-DFSRQuotaSize -Path 'S:\DFSR-Folder'
-      Will give you the quota to the screen.
+      Will give you the quota to the screen and add it to a temp file.
 
       .EXAMPLE 
-      Get-DFSRQuotaSize -Path 'S:\DFSR-Folder' -LogFolder c:\temp\Logs
+      Get-DFSRQuotaSize -Path 'S:\DFSR-Folder' -$LogFile c:\temp\Log.txt
       Will give you the quota to a file in the folder you provide
 
-      .EXAMPLE
-      Get-DFSRQuotaSize -Path 'S:\DFSR-Folder' -LogFolder c:\temp\Logs -verbose
-      Will give you the quota to a file in the folder you provide.  It will also output the path.
   #>
   
   [CmdletBinding(SupportsPaging)]
@@ -25,11 +25,22 @@ function Get-DFSRQuotaSize
     [string]$FullPath,
     
     [Parameter(Position = 1)]
-    [string]$LogFolder = $null
+    [AllowNull()]
+    [ValidateScript({
+          If($_ -notmatch '.csv')
+          {
+            $true
+          }
+          Else
+          {
+            Throw 'Output file can not be a .csv'
+          }
+    })][String]$LogFile = $null
   )
 
+  $NewLine = "`n"
   $DateNow = Get-Date -UFormat %Y%m%d-%S
-  $LogFile = (('{0}\{1}-DfsrQuota.txt' -f $LogFolder, $DateNow))
+  $LogFile = [String]$($LogFile.Replace('.',('_{0}.' -f $DateNow)))
 
   $Big32 = Get-ChildItem -Path $FullPath -Recurse |
   Sort-Object -Property length -Descending |
@@ -37,21 +48,39 @@ function Get-DFSRQuotaSize
   Measure-Object -Property length -Sum
   $DfsrQuota = $Big32.sum /1GB
 
-  $OutputInformation = ('The path tested: {0}.  The recommended Quota size is {1:n2} GB' -f $FullPath, $DfsrQuota)
-  Write-Output -InputObject $OutputInformation
-
-  if($LogFolder)
+  if($DfsrQuota -gt 1)
   {
-    ('Raw Full Path = {0}' -f $FullPath) | Out-File -FilePath $LogFile
-    ('Raw Quota = {0}' -f $DfsrQuota) | Out-File -FilePath $LogFile -Append
-    ('Username = {0}' -f $env:USERNAME) | Out-File -FilePath $LogFile -Append
-    Write-Output -InputObject ('The log can be found: {0}' -f  $LogFile)
+    $OutputInformation = ('The recommended Quota size is {0:n2} GB.' -f $DfsrQuota)
+    #Write-Output -InputObject $OutputInformation
+  }
+  else
+  {
+    $DfsrQuota = $Big32.sum /1MB
+    $OutputInformation = ('The recommended Quota size is {0:n2} MB.' -f $DfsrQuota)
+  }
+  if(-not $LogFile)
+  {
+    $LogFile = New-TemporaryFile
+  }
+  if(-not $(Test-Path -Path $LogFile))
+  {
+    $null = New-Item -Path $LogFile -ItemType File -Force
   }
 
-  Write-Verbose -Message ('Log file = {0} ' -f $LogFile)
-  Write-Verbose -Message ('Raw Full Path = {0}' -f $FullPath)
-  Write-Verbose -Message ('Raw Quota = {0}' -f $DfsrQuota)
+  
+  ('The path tested: {0}.' -f $FullPath) | Tee-Object  -FilePath $LogFile
+  
+  $OutputInformation | Tee-Object  -FilePath $LogFile -Append
+  
+  ('Raw Quota = {0:n4}' -f $DfsrQuota) | Tee-Object -FilePath $LogFile -Append
+  
+  Write-Output -InputObject ('The log can be found: {0}{1}' -f  $LogFile, $NewLine)
 }
+
+
+Get-DFSRQuotaSize -FullPath "$env:HOMEDRIVE\Temp"
+
+
 
 
 
