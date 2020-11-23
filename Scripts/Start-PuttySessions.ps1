3e5bdaa84a83b383a6f32ca244a4511e0499689a
Erik JA <OgJAkFy8@users.noreply.github.com>
2020-11-22 20:56:08 -0500

    Testing Putty

---
+++ Scripts/Start-PuttySessions.ps1
@@ -0,0 +1,47 @@
+﻿function Start-PuttySessions
+{
+  <#
+    .SYNOPSIS
+    Short Description
+    .DESCRIPTION
+    Detailed Description
+    .EXAMPLE
+    Start-Something
+    explains how to use the command
+    can be multiple lines
+    .EXAMPLE
+    Start-Something
+    another example
+    can have as many examples as you like
+  #>
+  [CmdletBinding()]
+  param
+  (
+    [Parameter(Mandatory=$false, Position=0)]
+    [System.String]
+    $LoginName = 'pi',
+    [Parameter(Mandatory=$false, Position=1)]
+    [System.String]
+    $PuttyPath = 'C:\Users\New User\OneDrive\Downloads\putty.exe'
+  )
+  $PuTTYSessions = Get-ItemProperty -Path HKCU:\Software\Simontatham\PuTTY\Sessions\*
+  function Start-PuttySession
+  {
+    param
+    (
+      [Object]
+      [Parameter(Mandatory=$true, ValueFromPipeline=$true, HelpMessage="Data to process")]
+      $InputObject
+    )
+    process
+    {
+      & $PuttyPath -load $InputObject.pschildname  ('{0}@{1}' -f $LoginName, $InputObject.hostname)
+    }
+  }
+  $MySession = $PuTTYSessions |
+  Select-Object -Property pschildname, hostname |
+  Out-GridView -PassThru
+  $MySession | Start-PuttySession
+}
+
+Start-PuttySessions
---
+++ Scripts/Test-OpenPort.ps1
@@ -0,0 +1,50 @@
+﻿#requires -Version 3.0 -Modules NetTCPIP
+function Test-OpenPort 
+{
+  <# 
+ 
+      .SYNOPSIS
+      Test-OpenPort is an advanced Powershell function. Test-OpenPort acts like a port scanner. 
+ 
+      .DESCRIPTION
+      Uses Test-NetConnection. Define multiple targets and multiple ports. 
+ 
+      .PARAMETER
+      Target
+      Define the target by hostname or IP-Address. Separate them by comma. Default: localhost 
+ 
+      .PARAMETER
+      Port
+      Mandatory. Define the TCP port. Separate them by comma. 
+ 
+      .EXAMPLE
+      Test-OpenPort -Target sid-500.com,cnn.com,10.0.0.1 -Port 80,443 
+ 
+      .NOTES
+      Author: Patrick Gruenauer
+      Web:
+      https://sid-500.com  
+  #>
+  param
+  (
+    [Parameter(Position = 0)]
+    [string]$Targets = 'localhost',
+    [Parameter(Mandatory, Position = 1, Helpmessage = 'Enter Port Numbers. Separate them by comma.')]
+    [Object]$Ports
+  )
+  $result = @()
+  foreach ($Target in $Targets)
+  {
+    foreach ($Port in $Ports)
+    {
+      $TestResult = Test-NetConnection -ComputerName $Target -Port $Port -WarningAction SilentlyContinue
+      $result += New-Object -TypeName PSObject -Property ([ordered]@{
+          'Target'      = $TestResult.ComputerName
+          'RemoteAddress' = $TestResult.RemoteAddress
+          'Port'        = $TestResult.RemotePort
+          'Status'      = $TestResult.tcpTestSucceeded
+      })
+    }
+  }
+  Write-Output -InputObject $result
+}
