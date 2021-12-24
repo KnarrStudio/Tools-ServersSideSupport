58fc26ae1dd7a9d6742d9c366ce3c76087508e57
Erik JA <OgJAkFy8@users.noreply.github.com>
2021-12-24 07:59:40 -0600

    Update Export-PuTTYSessions.ps1

--- Modules/PuttyTools/Export-PuTTYSessions.ps1
+++ Modules/PuttyTools/Export-PuTTYSessions.ps1
@@ -1,72 +1,97 @@
 ﻿function Export-PuTTYSessions #Completed
 {
+  [CmdletBinding(PositionalBinding = $true)]
   param
   (
-    [Parameter(Mandatory = $false,Position = 0)]
-    [string]
-    $SessionName = '*',
-    [string]$outputPath = ".\",
+    [Parameter(Position = 0,ParameterSetName = 'Session Set')]
+    [string]$SessionName = '*',
+    [Parameter(Mandatory = $false,Position = 1,ParameterSetName = 'Session Set')]
+    [Parameter(Mandatory = $false,Position = 0,ParameterSetName = 'Bundle Set')]
+    [string]$OutputPath = '.',
+    [Parameter(Position = 1,ParameterSetName = 'Bundle Set')]
     [Switch]$Bundle
   )
-  function script:New_RegistryFile
-  {
-    <#
-        .SYNOPSIS
-        Creates new file
-    #>
-    param
-    (
-      [Parameter(Mandatory = $true, Position = 0)]
-      [String]$FileName,
-      [Parameter(Mandatory = $true)]
-      [String]$outputPath 
-    )
-    $FileRegHeader = 'Windows Registry Editor Version 5.00'  
-    if(Test-Path -Path $outputPath)
+  
+  Begin{
+    $PuttyRegPath = 'HKCU:\Software\Simontatham\PuTTY\Sessions\'
+    $PuTTYSessions = ((Get-Item -Path ('{0}{1}' -f $PuttyRegPath, $SessionName)).Name)
+
+    if(-not (Test-Path -Path $OutputPath))
     {
-      $outputfile = $('{0}\{1}.reg' -f $outputPath, $FileName)
+      Write-Warning -Message ('OutputPath not found. Save to {0}? ' -f "$env:userprofile\Desktop")
+      if((Read-Host -Prompt 'Y/[N]').ToUpper() -eq 'Y')
+      {
+        $OutputPath = '.'
+      }
+      else
+      {
+        Write-Warning -Message 'Path not found. Create path and run again. '
+        Break
+      }
     }
-    if(Test-Path -Path $outputfile)
+
+    function New_RegistryFile
     {
-      $outputfile = $outputfile.Replace('.reg',('({0}).reg' -f (Get-Date -Format yyMMdd)))
+      <#
+          .SYNOPSIS
+          Creates new file
+      #>
+      param
+      (
+        [Parameter(Mandatory, Position = 0)]
+        [String]$FileName,
+        [Parameter(Mandatory)]
+        [String]$OutputPath 
+      )
+      $FileRegHeader = 'Windows Registry Editor Version 5.00'  
+      if(Test-Path -Path $OutputPath)
+      {
+        $outputfile = $('{0}\{1}.reg' -f $OutputPath, $FileName)
+      }
+      if(Test-Path -Path $outputfile)
+      {
+        $outputfile = $outputfile.Replace('.reg',('({0}).reg' -f (Get-Date -Format yyMMdd)))
+      }
+      $FileRegHeader | Out-File -FilePath $outputfile -Force
+      Return $outputfile
     }
-    $FileRegHeader | Out-File -FilePath $outputfile -Force
-    Return $outputfile
-  }
-  function script:Export_SessionToFile
-  {
-    <#
-        .SYNOPSIS
-        Export Reg Session to file
-    #>
-    param
-    (
-      [Parameter(Mandatory = $true, Position = 0)]
-      [String]$outputfile ,
-      [Parameter(Mandatory = $true, Position = 1)]
-      [String]$item
-    )
-    ('[{0}]' -f $item) | Out-File -FilePath $outputfile -Append  # Output session header to file
-    Get-ItemProperty -Path ('HKCU:{0}' -f $($item.TrimStart('HKEY_CURRENT_USER'))) | Out-File -FilePath $outputfile  -Append
+
+    function Export_SessionToFile
+    {
+      <#
+          .SYNOPSIS
+          Export Reg Session to file
+      #>
+      param
+      (
+        [Parameter(Mandatory, Position = 0)]
+        [String]$outputfile ,
+        [Parameter(Mandatory, Position = 1)]
+        [String]$item
+      )
+      ('[{0}]' -f $item) | Out-File -FilePath $outputfile -Append  # Output session header to file
+      Get-ItemProperty -Path ('HKCU:{0}' -f $($item.TrimStart('HKEY_CURRENT_USER'))) | Out-File -FilePath $outputfile  -Append
+    }
   }
 
-  $PuttyRegPath = 'HKCU:\Software\Simontatham\PuTTY\Sessions\'
-  $PuTTYSessions = ((Get-Item -Path ('{0}{1}' -f $PuttyRegPath, $SessionName)).Name)
-  if(-not $Bundle)
-  {
-    foreach($item in $PuTTYSessions)
+  Process{
+    if(-not $Bundle)
     {
-      $itemName = Split-Path -Path $item -Leaf
-      Export_SessionToFile -outputfile $(New_RegistryFile  -FileName $itemName -outputPath $outputPath ) -item $item
+      foreach($item in $PuTTYSessions)
+      {
+        $itemName = Split-Path -Path $item -Leaf
+        Export_SessionToFile -outputfile $(New_RegistryFile  -FileName $itemName -outputPath $OutputPath ) -item $item
+      }
+    }
+    else
+    {
+      $outputfile = New_RegistryFile -FileName 'PuttyBundle' -outputPath $OutputPath 
+      foreach($item in $PuTTYSessions)
+      {
+        $itemName = Split-Path -Path $item -Leaf
+        Export_SessionToFile -outputfile $outputfile -item $item
+      }
     }
   }
-  else
-  {
-    $outputfile = New_RegistryFile -FileName 'PuttyBundle' -outputPath $outputPath 
-    foreach($item in $PuTTYSessions)
-    {
-      $itemName = Split-Path -Path $item -Leaf
-      Export_SessionToFile -outputfile $outputfile -item $item
-    }
-  }
+  End{}
 }
